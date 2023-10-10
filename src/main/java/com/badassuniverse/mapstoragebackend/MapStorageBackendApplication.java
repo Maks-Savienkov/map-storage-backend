389a388128b72f52c74b2c36d9c88ee240841991
Savienkov Maksym <froggt25@gmail.com>
2023-10-10 13:36:48 +0300

    Init commit

---
+++ src/main/java/com/badassuniverse/mapstoragebackend/MapStorageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.badassuniverse.mapstoragebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStorageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapStorageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/badassuniverse/mapstoragebackend/MapStorageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badassuniverse.mapstoragebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStorageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
