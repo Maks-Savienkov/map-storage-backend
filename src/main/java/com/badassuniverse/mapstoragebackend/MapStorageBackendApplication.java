9bb9428e51d85e746f2d036babaab3b17b06f32c
Maks Savienkov <savyenkov.maksym@vu.cdu.edu.ua>
2023-12-19 09:08:26 +0200

    Add configurations

--- src/main/java/com/badassuniverse/mapstoragebackend/MapStorageBackendApplication.java
+++ src/main/java/com/badassuniverse/mapstoragebackend/MapStorageBackendApplication.java
@@ -1,13 +1,14 @@
 package com.badassuniverse.mapstoragebackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Map Storage API", version = "1.0"))
 public class MapStorageBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MapStorageBackendApplication.class, args);
     }
-
 }
